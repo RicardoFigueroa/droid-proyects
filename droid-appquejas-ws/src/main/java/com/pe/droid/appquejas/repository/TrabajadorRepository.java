64b58a3c8f301e015874b5e957cd03582d36b031
Ricardo <andres@rfigueroa.com>
2015-12-03 01:28:54 -0500

    add login

--- droid-appquejas-ws/src/main/java/com/pe/droid/appquejas/repository/TrabajadorRepository.java
+++ droid-appquejas-ws/src/main/java/com/pe/droid/appquejas/repository/TrabajadorRepository.java
@@ -1,14 +1,10 @@
 package com.pe.droid.appquejas.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.pe.droid.appquejas.domain.Trabajador;
 import com.pe.droid.appquejas.domain.TrabajadorPK;
 
 public interface TrabajadorRepository extends CrudRepository<Trabajador, TrabajadorPK> {
-
-	  @Query(value = "SELECT * FROM trabajador WHERE id_trabajador = ?0", nativeQuery = true)
-	  Trabajador findByIdTrabajador(Long id_trabajador);
 	
 }
